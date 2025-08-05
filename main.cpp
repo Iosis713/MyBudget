81feea944dc397d291ffa27f5edf6004ae0e1410
Bartosz Lozinski <lozinski.bart@gmail.com>
2025-08-05 19:57:12 +0200

    vscode setup

--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
 #include <QApplication>
 #include <QLabel>
+#include <iostream>
 
+class Foo
+{
+  public:
+    void bar() { std::cout << "Unformatted!"; }
+};
+
+int main()
+{
+    Foo f;
+    f.bar();
+    return 0;
+}
+
+/*
 int main(int argc, char* argv[])
 {
     QApplication app(argc,argv);
 
     QLabel label("Hello in the MyBudget app!");
     label.show();
-    
+
+        std::cout <<   "someting\n";
+
     return app.exec();
-}
+}*/
